6f56f71ddfe519c2269dd61a0e4c17658ceed156
Sinan Shaban <sinan.zikri@gmail.com>
2018-12-07 22:53:00 +0200

    add color entity

---
+++ AutoService/Models/CarColorRepository.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AutoService.Models
+{
+    class CarColorRepository
+    {
+        public static string connectionString = System.Configuration
+            .ConfigurationManager
+            .ConnectionStrings["AutoServiceConnectionString"]
+            .ConnectionString;
+
+        public static CarColor Get(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand("SELECT id as ID, name as Name FROM colors where id = @id", con))
+                {
+                    command.Parameters.Add("@id", SqlDbType.Int);
+                    command.Parameters["@id"].Value = id;
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            return new CarColor(reader.GetInt32(0), reader.GetString(1).Trim());
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+        public static List<CarColor> GetAll()
+        {
+            List<CarColor> colors = new List<CarColor>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand("SELECT id as ID, name as Name FROM colors", con))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            colors.Add(new CarColor(reader.GetInt32(0), reader.GetString(1).Trim()));
+                        }
+                    }
+                }
+            }
+
+            return colors;
+        }
+
+        public static void Add(string name)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand("INSERT INTO colors (name) VALUES(@name)", con))
+                {
+                    command.Parameters.Add("@name", SqlDbType.NVarChar);
+                    command.Parameters["@name"].Value = name;
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void Update(int id, string name)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand("UPDATE colors SET name = @name WHERE id = @id", con))
+                {
+                    command.Parameters.Add("@id", SqlDbType.Int);
+                    command.Parameters["@id"].Value = id;
+                    command.Parameters.Add("@name", SqlDbType.NVarChar);
+                    command.Parameters["@name"].Value = name;
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static void Remove(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand("DELETE FROM colors WHERE id = @id", con))
+                {
+                    command.Parameters.Add("@id", SqlDbType.Int);
+                    command.Parameters["@id"].Value = id;
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
